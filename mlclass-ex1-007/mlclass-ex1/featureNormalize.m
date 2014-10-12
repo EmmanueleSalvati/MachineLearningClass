82704cdf4f23d291eb74e3705125d3b6bfa21ed2
Emmanuele <emmanuele.salvati@gmail.com>
2014-10-12 15:30:41 -0400

    Start extra credits P1

--- mlclass-ex1-007/mlclass-ex1/featureNormalize.m
+++ mlclass-ex1-007/mlclass-ex1/featureNormalize.m
@@ -1,5 +1,5 @@
 function [X_norm, mu, sigma] = featureNormalize(X)
-%FEATURENORMALIZE Normalizes the features in X 
+%FEATURENORMALIZE Normalizes the features in X
 %   FEATURENORMALIZE(X) returns a normalized version of X where
 %   the mean value of each feature is 0 and the standard deviation
 %   is 1. This is often a good preprocessing step to do when
@@ -24,11 +24,11 @@ sigma = zeros(1, size(X, 2));
 %               each feature. 
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
-%       
+%
 
-
-
-
+mu = mean(X);
+sigma = std(X);
+X_norm = (X .- mu) ./ sigma;
 
 
 
