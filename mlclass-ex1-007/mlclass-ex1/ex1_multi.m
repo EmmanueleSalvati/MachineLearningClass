34fe8227b7612e315997b64e5603d0357b2f3ec5
Emmanuele <emmanuele.salvati@gmail.com>
2014-10-13 20:26:37 -0400

    All but one of the optional multivariate problems

--- mlclass-ex1-007/mlclass-ex1/ex1_multi.m
+++ mlclass-ex1-007/mlclass-ex1/ex1_multi.m
@@ -60,13 +60,13 @@ X = [ones(m, 1) X];
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
 %               code that runs gradient descent with a particular
-%               learning rate (alpha). 
+%               learning rate (alpha).
 %
-%               Your task is to first make sure that your functions - 
-%               computeCost and gradientDescent already work with 
+%               Your task is to first make sure that your functions -
+%               computeCost and gradientDescent already work with
 %               this starter code and support multiple variables.
 %
-%               After that, try running gradient descent with 
+%               After that, try running gradient descent with
 %               different values of alpha and see which one gives
 %               you the best result.
 %
@@ -85,7 +85,7 @@ fprintf('Running gradient descent ...\n');
 alpha = 0.01;
 num_iters = 400;
 
-% Init Theta and Run Gradient Descent 
+% Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
@@ -100,11 +100,29 @@ fprintf('Theta computed from gradient descent: \n');
 fprintf(' %f \n', theta);
 fprintf('\n');
 
+% Trying different values of alpha
+theta1 = zeros(3, 1);
+theta2 = zeros(3, 1);
+theta3 = zeros(3, 1);
+[theta1, J1] = gradientDescentMulti(X, y, theta1, 0.3, num_iters);
+[theta2, J2] = gradientDescentMulti(X, y, theta2, 0.1, num_iters);
+[theta3, J3] = gradientDescentMulti(X, y, theta3, 0.003, num_iters);
+figure;
+% plot(1:50, J1(1:50), 'b');
+% hold on;
+% plot(1:50, J2(1:50), 'r');
+% hold on;
+plot(1:50, J3(1:50), 'k');
+
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
+house = [1650, 3];
+house = (house - mu) ./ sigma;
+house = [1 house];
+price = house * theta;
 
 
 % ============================================================
@@ -120,12 +138,12 @@ pause;
 fprintf('Solving with normal equations...\n');
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: The following code computes the closed form 
+% Instructions: The following code computes the closed form
 %               solution for linear regression using the normal
-%               equations. You should complete the code in 
+%               equations. You should complete the code in
 %               normalEqn.m
 %
-%               After doing so, you should complete this code 
+%               After doing so, you should complete this code
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
 
@@ -150,7 +168,8 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-
+house = [1, 1650, 3];
+price = house * theta;
 
 % ============================================================
 
--- mlclass-ex1-007/mlclass-ex1/gradientDescentMulti.m
+++ mlclass-ex1-007/mlclass-ex1/gradientDescentMulti.m
@@ -11,25 +11,26 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
+    temp_theta = theta;
+    % calculate one theta_j per feature
+    for j = 1:size(X, 2),
+        theta(j) = temp_theta(j) - 1 / m * sum((X * theta - y)' * X(:, j));
+    end
 
 
-
-
-
-
-
+    temp_theta = theta;
 
 
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
