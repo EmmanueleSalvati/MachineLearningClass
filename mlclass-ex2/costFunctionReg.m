bddba065b065237f8a2afc3edfe2947dab2d0655
Emmanuele <emmanuele.salvati@gmail.com>
2014-11-20 22:55:44 -0500

    Complete ex2_reg
    Finally completed the Programming Exercise 2

--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -2,12 +2,12 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -17,10 +17,34 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% UNREGULARIZED COST FUNCTION
+my_sum = 0;
+for i = 1:m
+    my_sum += -y(i) * log(sigmoid(theta' * X(i, :)')) ...
+        - (1 - y(i)) * log(1 - sigmoid(theta' * X(i, :)'));
+end;
+J = J + 1/m * my_sum;
 
+% REGULARIZATION TERM OF THE COST FUNCTION
+reg_sum = 0;
+for j = 2:size(theta)
+    reg_sum += theta(j)^2;
+end;
 
+J = J + lambda/(2*m) * reg_sum;
 
+%%%%%%%%%%%%%%%
+% UNREGULARIZED GRADIENT OF THE COST FUNCTION
+j_sum = zeros(size(grad));
+for j = 1:size(grad)
+    for i = 1:m
+        j_sum(j) = j_sum(j) + (sigmoid(theta' * X(i, :)') - y(i)) * X(i, j);
+    end;
+end;
 
+temp_vec = [0; ones(size(theta, 1)-1, 1)];
+
+grad = 1/m * j_sum + lambda/m * (theta .* temp_vec);
 
 % =============================================================
 
